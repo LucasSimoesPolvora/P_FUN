4ef9dfaf5374705913d3788961884de7b73ed3d4
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-09-06 13:38:39 +0200

    Read the csv file located in the bin

--- WorldPopulation/MainWindow.xaml.cs
+++ WorldPopulation/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace WorldPopulation
         {
             InitializeComponent();
 
-            ReadCsvFile();
+            List<CountryPopulation> countryPop = ReadCsvFile();
 
             Loaded += (s, e) =>
             {
@@ -34,53 +34,70 @@ namespace WorldPopulation
                 double[] dataY = { 1, 4, 9, 16, 25 };
                 WpfPlot1.Plot.Add.Scatter(dataX, dataY);
                 WpfPlot1.Refresh();
+                
             };
         }
 
-        public void ReadCsvFile()
+        /// <summary>
+        /// This method will read the csv file and return a list of class with the results
+        /// </summary>
+        public List<CountryPopulation> ReadCsvFile()
         {
+            // 
             string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\world_population.csv";
             
             using(StreamReader reader = new StreamReader(path))
             {
                 List<CountryPopulation> country = new List<CountryPopulation>();
-                while (!reader.EndOfStream)
+                try
                 {
-                    string line = reader.ReadLine();
-                    string[] values = line.Split(',');
-
-                    Int32.TryParse(values[0], out int rank);
-                    Int32.TryParse(values[5], out int p22);
-                    Int32.TryParse(values[6], out int p20);
-                    Int32.TryParse(values[7], out int p15);
-                    Int32.TryParse(values[8], out int p10);
-                    Int32.TryParse(values[9], out int p00);
-                    Int32.TryParse(values[10], out int p90);
-                    Int32.TryParse(values[11], out int p80);
-                    Int32.TryParse(values[12], out int p70);
-
-                    CountryPopulation c = new CountryPopulation
+                    // Reading the csv file
+                    while (!reader.EndOfStream)
                     {
-                        Rank = rank,
-                        CCA3 = values[1],
-                        Country = values[2],
-                        Capital = values[3],
-                        Continent = values[4],
-                        Population2022 = p22,
-                        Population2020 = p20,
-                        Population2015 = p15,
-                        Population2010 = p10,
-                        Population2000 = p00,
-                        Population1990 = p90,
-                        Population1980 = p80,
-                        Population1970 = p70,
-                    };
-                    country.Add(c);
+                        string line = reader.ReadLine();
+                        string[] values = line.Split(',');
+
+                        // Converting string to int, if not done can do types errors
+                        Int32.TryParse(values[0], out int rank);
+                        Int32.TryParse(values[5], out int p22);
+                        Int32.TryParse(values[6], out int p20);
+                        Int32.TryParse(values[7], out int p15);
+                        Int32.TryParse(values[8], out int p10);
+                        Int32.TryParse(values[9], out int p00);
+                        Int32.TryParse(values[10], out int p90);
+                        Int32.TryParse(values[11], out int p80);
+                        Int32.TryParse(values[12], out int p70);
+
+                        CountryPopulation c = new CountryPopulation
+                        {
+                            Rank = rank,
+                            CCA3 = values[1],
+                            Country = values[2],
+                            Capital = values[3],
+                            Continent = values[4],
+                            Population2022 = p22,
+                            Population2020 = p20,
+                            Population2015 = p15,
+                            Population2010 = p10,
+                            Population2000 = p00,
+                            Population1990 = p90,
+                            Population1980 = p80,
+                            Population1970 = p70,
+                        };
+                        country.Add(c);
+                    }
+                }
+                catch 
+                {
+                    MessageBox.Show("Unable to read file, check if there is an error in the file.", "Save error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
                 Console.ReadLine();
+
+                return country;
             }
         }
 
+        // Creating the class that will contain the csv
         public class CountryPopulation
         {
             public int Rank { get; set; }
